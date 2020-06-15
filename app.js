d075918c32d2eb03603b303cc97c96cc41533b60
Carson Keller <carsonjkeller@gmail.com>
2020-06-15 11:24:28 -0600

    Added dotenv package and dependencies and implemented them in necessary files.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-require('dotenv').config ;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
